fb23f2a76bdb44e59272a8f2a4aebe77b0fc6737
Alex Mann <atmann@alexs-mbp.usoh.ibm.com>
2018-05-14 10:35:23 -0400

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -25,10 +26,6 @@ app.post('/todos', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('started on port 3000')
-});
-
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
@@ -36,5 +33,29 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+// GET /todos/12341234
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        } else {
+            res.status(200).send({todo});
+        }
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.listen(3000, () => {
+    console.log('started on port 3000')
+});
+
+
 
 module.exports = {app};
