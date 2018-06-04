b15faea31b172009b4d58a3b221a63b07f54fa2d
Alex Mann <atmann@Alexs-MacBook-Pro.local>
2018-06-04 16:25:34 -0400

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,69 +1,83 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
 
-var data = {
-    id: 10
-};
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var token = jwt.sign(data, '123abc'); //data is the data, '123abc' is the SECRET. the SALT
-console.log(token);
+var hashedPassword = '$2a$10$3xww6tGpHwffjpQIwSNMsePTcD/MzLEX870e7GRZdBXzl91N/jUmy';
 
-
-
-var decoded = jwt.verify(token, '123abc'); //ONLY WHEN THE TOKEN IS UNALTERED AND THE SECRET IS THE SAME WILL WE GETTHE RIGHT RESULT
-//THIS MIMICS THE CODE BELOW. JWT DOES THE WORK FOR YOU
-console.log('decoded: ', decoded);
-
-
-
-
-
-
-
-
-
-
-// var message = 'I am user number 3';
-// var hash = SHA256(message).toString();
-
-// console.log(`message: ${message}`);
-// console.log(`hash: ${hash}`);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
 
 // var data = {
-//     id: 4
+//     id: 10
 // };
-// var token = {
-//     data: data,
-//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// }
+
+// var token = jwt.sign(data, '123abc'); //data is the data, '123abc' is the SECRET. the SALT
+// console.log(token);
 
 
-// token.data.id = 5;
-// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var decoded = jwt.verify(token, '123abc'); //ONLY WHEN THE TOKEN IS UNALTERED AND THE SECRET IS THE SAME WILL WE GETTHE RIGHT RESULT
+// //THIS MIMICS THE CODE BELOW. JWT DOES THE WORK FOR YOU
+// console.log('decoded: ', decoded);
 
 
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-// if (resultHash === token.hash) {
-//     console.log('data was not changed');
-// } else {
-//     console.log('data was changed. do not trust');
-// }
 
-/*
-so, why am I hashing the data object inside token? 
-imagine a scenario where the client gets {data}, sees the id is 4, and changes it to 5
-then sends a delete for id: 5. 
-we don't want to allow that
-so instead, when we get that we're operating on id: 4, we hash that object
-since hashing is a one-way algorithm, if the data is changed on the client side the hashes won't match and we can stop all execution
 
-now, that's still not foolproof. the user could take the token and change the hash value to that of id: 5. that's where "salting" comes in
-if you have some secret salt value that you add to your hashes, only you will know which hashes are correct
-the salt MUST be secret
 
-the example of this is seen on lines 18 and 19. pretend these are the actions that an attacker is taking
-they have the token and change the id to 5, and hash it. then send it all back
-on our end we will take the hash they give us in the token and compare it to the salted hash
-since they don't have the salt, we know something changed. the original salt was lost when they rehashed. so we don't accept
-*/
+
+
+
+
+
+// // var message = 'I am user number 3';
+// // var hash = SHA256(message).toString();
+
+// // console.log(`message: ${message}`);
+// // console.log(`hash: ${hash}`);
+
+// // var data = {
+// //     id: 4
+// // };
+// // var token = {
+// //     data: data,
+// //     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// // }
+
+
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+
+// // var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// // if (resultHash === token.hash) {
+// //     console.log('data was not changed');
+// // } else {
+// //     console.log('data was changed. do not trust');
+// // }
+
+// /*
+// so, why am I hashing the data object inside token? 
+// imagine a scenario where the client gets {data}, sees the id is 4, and changes it to 5
+// then sends a delete for id: 5. 
+// we don't want to allow that
+// so instead, when we get that we're operating on id: 4, we hash that object
+// since hashing is a one-way algorithm, if the data is changed on the client side the hashes won't match and we can stop all execution
+
+// now, that's still not foolproof. the user could take the token and change the hash value to that of id: 5. that's where "salting" comes in
+// if you have some secret salt value that you add to your hashes, only you will know which hashes are correct
+// the salt MUST be secret
+
+// the example of this is seen on lines 18 and 19. pretend these are the actions that an attacker is taking
+// they have the token and change the id to 5, and hash it. then send it all back
+// on our end we will take the hash they give us in the token and compare it to the salted hash
+// since they don't have the salt, we know something changed. the original salt was lost when they rehashed. so we don't accept
+// */
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -75,6 +76,30 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        // user.password
+        // hash it with bcrypt.hash
+        // set it on user.password
+        // then call next
+        // call genSalt
+        // call hash
+        // inside callback for hash you need to add user.password = hash and next();
+        // once this is done, add a new user and password should be hashed in the database
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model('User', UserSchema);
 
